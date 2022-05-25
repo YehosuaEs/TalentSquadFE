b6b3f58a398153696292f60bdbc90b5803c8a0cf
yehosuaES <yehosuaes@gmail.com>
2022-05-25 17:56:26 +0200

    start project and style components install

---
+++ nft_pokemon/src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+import Loading from './components/loading/loading';
+
+function App() {
+  return (
+   <>
+    <Loading/>
+   </>
+  )
+}
+
+export default App;
---
+++ nft_pokemon/src/components/loading/loading.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+
+
+function Loading () {
+
+    return (
+        <Fragment>
+                <h1>
+                    Hola
+                </h1>
+        </Fragment>
+    )
+}
+
+export default Loading;
